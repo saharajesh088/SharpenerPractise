344936f0daa0255abaa636818cf30fd90a4bb1da
Rajesh Saha <saharajesh599@gmail.com>
2023-01-25 16:31:03 +0530

    task 9

--- main.js
+++ main.js
@@ -1,67 +1,63 @@
-var form = document.getElementById("addForm");
-var itemList = document.getElementById("items");
-var filter = document.getElementById("filter");
+let form = document.getElementById("addForm");
+let itemList = document.getElementById("items");
+let filter = document.getElementById("filter");
+let btn = document.getElementById("submitBtn");
 
-// Form submit event
 form.addEventListener("submit", addItem);
-// Delete event
-itemList.addEventListener("click", removeItem);
-// Filter event
-filter.addEventListener("keyup", filterItems);
+itemList.addEventListener("click",removeItem);
+filter.addEventListener("keyup",filterItems);
+//add item
+function addItem(e){
+    e.preventDefault();
+    let newItem = document.getElementById("item");
+    let li = document.createElement("li");
+    li.className="list-group-item";
+    li.appendChild(document.createTextNode(newItem.value));
+    li.appendChild(document.createTextNode(inputEle.value));
 
-// Add item
-function addItem(e) {
-  e.preventDefault();
+    let editBtn = document.createElement("button");
+    editBtn.className="btn btn-primary float-right btn-sm";
+    editBtn.appendChild(document.createTextNode("E"));
+    li.appendChild(editBtn);
 
-  // Get input value
-  var newItem = document.getElementById("item").value;
-
-  // Create new li element
-  var li = document.createElement("li");
-  // Add class
-  li.className = "list-group-item";
-  // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
-
-  // Create del button element
-  var deleteBtn = document.createElement("button");
-
-  // Add classes to del button
-  deleteBtn.className = "btn btn-danger btn-sm float-right delete";
-
-  // Append text node
-  deleteBtn.appendChild(document.createTextNode("X"));
-
-  // Append button to li
-  li.appendChild(deleteBtn);
-
-  // Append li to list
-  itemList.appendChild(li);
+    let deleteBtn = document.createElement("button");
+    deleteBtn.className="btn btn-danger btn-sm  float-right delete";
+    deleteBtn.appendChild(document.createTextNode("X"));
+    li.appendChild(deleteBtn);
+    itemList.appendChild(li);
+    newItem.value='';
+    inputEle.value='';
+}
+//remove items
+function removeItem(e){
+    e.preventDefault();
+    //console.log(e.target);
+    if(e.target.classList.contains('delete')){
+        if(confirm("are you sure you want to delete....?")){
+            let li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }    
+    
+}
+//filter items
+function filterItems(e){
+    let text = e.target.value.toLowerCase();
+    let items = itemList.getElementsByTagName("li");
+    Array.from(items).forEach(function(item){
+        let itemName = item.firstChild.textContent;
+        let description = item.childNodes[1].textContent;
+        //console.log(itemName);
+        if(itemName.toLowerCase().indexOf(text) != -1|| description.toLowerCase().indexOf(text) != -1){
+            item.style.display="block";
+        }else{
+            item.style.display="none";
+        }
+    })
 }
 
-// Remove item
-function removeItem(e) {
-  if (e.target.classList.contains("delete")) {
-    if (confirm("Are You Sure?")) {
-      var li = e.target.parentElement;
-      itemList.removeChild(li);
-    }
-  }
-}
-
-// Filter Items
-function filterItems(e) {
-  // convert text to lowercase
-  var text = e.target.value.toLowerCase();
-  // Get lis
-  var items = itemList.getElementsByTagName("li");
-  // Convert to an array
-  Array.from(items).forEach(function (item) {
-    var itemName = item.firstChild.textContent;
-    if (itemName.toLowerCase().indexOf(text) != -1) {
-      item.style.display = "block";
-    } else {
-      item.style.display = "none";
-    }
-  });
-}
+//add input description
+let inputEle = document.createElement("input");
+inputEle.type="text";
+inputEle.className = "form-control mr-2";
+form.insertBefore(inputEle,btn);
