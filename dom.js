e2d21745d6d0c27ed9a139af4ec139c5e39bfaa9
Rajesh Saha <saharajesh599@gmail.com>
2023-01-25 14:02:00 +0530

    querySelector modified

--- dom.js
+++ dom.js
@@ -29,57 +29,78 @@
 
 // GETELEMENTSBYCLASSNAME 
 
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello 2';
-items[0].style.fontWeight = 'bold';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'yellow';
-items[2].style.fontWeight = 'bold';
-items[2].style.backgroundColor = 'green';
-items[3].style.fontWeight = 'bold';
-items[4].style.fontWeight = 'bold';
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello 2';
+// items[0].style.fontWeight = 'bold';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
+// items[2].style.fontWeight = 'bold';
+// items[2].style.backgroundColor = 'green';
+// items[3].style.fontWeight = 'bold';
+// items[4].style.fontWeight = 'bold';
 
 
 
 
 //items.style.backgroundColor = '#f4f4f4';
 
-for(var i=0; i < items.length;i++){
-    items[i].style.backgroundColor = 'f4f4f4';
+// for(var i=0; i < items.length;i++){
+//     items[i].style.backgroundColor = 'f4f4f4';
+// }
+
+// // GETELEMENTSBYTAGNAME
+
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'Hello 2';
+// li[1].style.fontWeight = 'bold';
+// li[1].style.backgroundColor = 'yellow';
+
+// items.style.backgroundColor = '#f4f4f4';
+
+// for(var i=0; i < items.length;i++){
+//     items[i].style.backgroundColor = 'f4f4f4';
+// }
+
+
+//QUERYSELECTOR
+
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+var input = document.querySelector('input');
+input.value = 'Hello World'
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value = "SEND"
+
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.background = 'green';
+
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display = 'none';
+
+
+//QUERYSELECTORALL
+
+var titles = document.querySelectorAll('.title');
+
+console.log(titles);
+titles[0].textContent = 'Hello';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+for(var i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor = 'green';
+    even[i].style.backgroundColor = '#ccc';
 }
-
-// GETELEMENTSBYTAGNAME
-
-var li = document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].textContent = 'Hello 2';
-li[1].style.fontWeight = 'bold';
-li[1].style.backgroundColor = 'yellow';
-
-items.style.backgroundColor = '#f4f4f4';
-
-for(var i=0; i < items.length;i++){
-    items[i].style.backgroundColor = 'f4f4f4';
-}
-
-
-
-
-var items = document.getElementsByClassName('tea-making-ingredient');
-console.log(ingredients);
-console.log(ingredients[1]);
-ingredients[1].textContent = 'milk';
-ingredients[0].style.fontWeight = 'bold';
-ingredients[1].style.fontWeight = 'bold';
-ingredients[1].style.backgroundColor = 'yellow';
-ingredients[2].style.fontWeight = 'bold';
-ingredients[2].style.backgroundColor = 'green';
-ingredients[3].style.fontWeight = 'bold';
-ingredients[4].style.fontWeight = 'bold';
-
-
-
 
